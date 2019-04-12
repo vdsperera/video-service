2248747ccdb40422d5bacda0b3e9970ac72016a4
Vidumini <heartyvds@gmail.com>
2019-04-12 12:38:26 +0530

    added post membership creation

--- source/memberships/models.py
+++ source/memberships/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.conf import settings
+from django.db.models.signals import post_save
+import stripe;
 
 # Create your models here.
 
@@ -29,6 +31,16 @@ class UserMembership(models.Model):
     def __str__(self):
         return self.user.username
 
+def post_save_membership_create(sender, instance, created, *args, **kwargs):
+    if created:
+        UserMembership.objects.get_or_created(user = instance)    
+    user_membership, created = UserMembership.objects.get_or_created(user = instance)
+    if user_membership.stripe_customer_id is None or user_membership.stripe_customer_id == '':
+        new_customer_id = stripe.Customer.create(email = instance.email)
+        user_membership.stripe_customer_id = new_customer_id['id']
+        user_membership.save()
+
+post_save.connect(post_save_membership_create, sender = settings.AUTH_USER_MODEL)
 class Subscription(models.Model):
     user_membership = models.ForeignKey(UserMembership, on_delete = models.CASCADE)
     stripe_subscription_id = models.CharField(max_length = 40)
